a3f5c8d7ab8756a41526617627f4fe7c00e3bcca
Anthony19-20 <barellitony13@gmail.com>
2025-12-29 13:33:42 +0100

    Delete Activité3_prems directory

--- Activité3_prems/Activite1_Fonction/script.js
+++
@@ -1,8 +0,0 @@
-function ttc(ht) {
-    var tva = 19.6;
-    var tvaTot = (ht * 19.6) / 100;
-    return tvaTot + ht;
-}
-//console.debug('La tva est de ' + tva);
-var prixTTC = ttc(100);
-console.debug('Le prix TTC est de ' + prixTTC);
